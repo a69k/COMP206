48e5ce5c49dca6ab36385434805fce9d2918cd2d
a04k <63ahmedkhaled@gmail.com>
2024-03-21 04:29:41 +0200

    Sheet 4 ~ lab fix

--- Sheet 4/script.js
+++ Sheet 4/script.js
@@ -1,99 +1,96 @@
+checkUser = () => {
+  const username = document.getElementById("user");
+  let userValid = false;
 
-checkUser=()=>{
-  username=document.getElementById("user").value;
-  usernamevalid=false;
+  if (username.value.length >= 6 && username.value.length <= 12 && username.value !== "") {
+    userValid = true;
+  } else {
+    const uLabel = document.getElementById("uLabel");
+    uLabel.style.color = 'red';
+  }
 
-if(username.length>=6&&username.length<=12&&username!="")
-usernamevalid=true
+  username.addEventListener("focus", () => {
+    uLabel.style.color = 'rgb(33, 247, 229)';
+  });
 
-
-if(!usernamevalid)
-{
-uLabel=document.getElementById("uLabel");
-uLabel.style.color='red';
-}
-un.addEventListener("focus",()=>{
-uLabel.style.color='black'
-
-})
-return usernamevalid
-}
-function correctpatt(ele)
-{
-  re=/[a-z]/&&/[A-Z]/
-  return re.test(ele)
-  
-}
-checkpasword=()=>
-{password=document.getElementById("pass").value
-passwordvalid=false
-if(password.length>=6&&correctpatt(password))
-passwordvalid=true
-if(!passwordvalid)
-{
-pLabel=document.getElementById("pLabel")
-pLabel.style.color='red'
-}
-pass.addEventListener("focus",()=>{
-pLabel.style.color='black'
-
-})
-return passwordvalid
-}
-checksemester=()=>{
-  semestervalid=false
-semesteroptions=document.querySelectorAll("option")
-if(semesteroptions[1].selected||semesteroptions[2].selected)
-semestervalid=true
-if(!semestervalid)
-sem.style.color="red"
-document.getElementById("sel").addEventListener("focus",()=>{
-sem.style.color='black'
-
-})
-return semestervalid
-}
-checkboxes=document.querySelectorAll("input[type=checkbox]")
-checkboxes[3].addEventListener("change",()=>{
-  if(checkboxes[3].checked)
-  window.alert("there may be potential spam")
-
-})
-
-document.forms[0].addEventListener("submit",function (e)
-{e.preventDefault()
-checkUser();
-checkpasword();
-checksemester();
-checkboxes=document.querySelectorAll("input[type=checkbox]")
-if(checkpasword()&&checksemester()&&checkUser())
-{
-textarea=document.createElement("textarea")
-document.forms[0].appendChild(textarea)
-textarea.setAttribute("rows","8")
-textarea.textContent=`name : ${username} \npassword : ${password}\nsemester: \n`
-semesteroptions=document.querySelectorAll("option")
-for( i=0;i<semesteroptions.length;i++)
-{
-if(semesteroptions[i].selected)
-textarea.textContent+=`${semesteroptions[i].value}\n`
-}
-checkboxes=document.querySelectorAll("input[type=checkbox]")
-for( i=0;i<checkboxes.length;i++)
-{
-if(checkboxes[i].checked)
-textarea.textContent+=`${checkboxes[i].value}\n`
+  return userValid;
 }
 
-textarea.addEventListener("click",()=>{
-  result = confirm("submit??!?!")
-  if(result)
-  console.log(`!!mogus!!`)
-})
+function correctpatt(ele) {
+  const re = /[a-z]/ && /[A-Z]/;
+  return re.test(ele);
+}
 
-textarea.addEventListener("mouseover",()=>{
-  result=confirm("submit??!?!")
-  if(result)
-    console.log(`!!mogus!!`)
-})}
-})
+checkPass = () => {
+  const password = document.getElementById("pass");
+  let passValid = false;
+
+  if (password.value.length >= 6 && correctpatt(password.value)) {
+    passValid = true;
+  } else {
+    const pLabel = document.getElementById("pLabel");
+    pLabel.style.color = 'red';
+  }
+
+  password.addEventListener("focus", () => {
+    pLabel.style.color = 'rgb(33, 247, 229)';
+  });
+  return passValid;
+}
+checkSem = () => {
+  const semOptions = document.querySelectorAll("option");
+  let semValid = false;
+
+  for (let i = 1; i < semOptions.length; i++) {
+    if (semOptions[i].selected) {
+      semValid = true;
+      break;
+    }
+  }
+
+  const semLabel = document.getElementById('sLabel');
+  if (!semValid) {
+    semLabel.style.color = "red";
+  } else {
+    semLabel.style.color = 'rgb(33, 247, 229)';
+  }
+
+  document.getElementById("sel").addEventListener("focus", () => {
+    semLabel.style.color = 'rgb(33, 247, 229)';
+  });
+
+  return semValid;
+}
+
+const checkboxes = document.querySelectorAll("input[type=checkbox]");
+
+checkboxes[3].addEventListener("change", () => {
+  if (checkboxes[3].checked) {
+    window.alert("You're Signing up for out newsletter");
+  }
+});
+
+document.forms[0].addEventListener("submit", (e) => {
+  e.preventDefault();
+  const checkboxes = document.querySelectorAll("input[type=checkbox]");
+
+  if (checkPass() && checkSem() && checkUser()) {
+    const textarea = document.createElement("textarea");
+    document.forms[0].appendChild(textarea);
+    textarea.setAttribute("rows", "8");
+    textarea.textContent = `name : ${document.getElementById("user").value} \npassword : ${document.getElementById("pass").value}\nsemester: `;
+
+    const semOptions = document.querySelectorAll("option");
+
+    for (let i = 0; i < semOptions.length; i++) {
+      if (semOptions[i].selected) {
+        textarea.textContent += `${semOptions[i].value}\n`;
+      }
+    }
+    for (let i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked) {
+        textarea.textContent += `${checkboxes[i].value}\n`;
+      }
+    }
+  }
+});
